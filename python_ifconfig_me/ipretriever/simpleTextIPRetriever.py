9ee73f58b90f61d3f5447b4f0512b546b8f9aa9a
catwang01 <edwardlericwzx@gmail.com>
2024-12-10 21:29:45 +0800

    refactor: update SimpleTextIPRetriever to inherit from CallbackIPRetriever and improve error message assertion

--- python_ifconfig_me/ipretriever/simpleTextIPRetriever.py
+++ python_ifconfig_me/ipretriever/simpleTextIPRetriever.py
@@ -1,30 +1,13 @@
 import logging
-import aiohttp
 
-from python_ifconfig_me.ipretriever.IPRetriever import IPObject, IPResultObject
-from python_ifconfig_me.ipretriever.IPRetriever import IPRetriever
-from python_ifconfig_me.ipretriever.IPRetriever import IPRetrieverContext
+from python_ifconfig_me.ipretriever.callbackIPRetriever import CallbackIPRetriever
 
 logger = logging.getLogger(__name__)
 
 
-class SimpleTextIPRetriever(IPRetriever):
+class SimpleTextIPRetriever(CallbackIPRetriever):
 
     def __init__(self, url: str, priority: int = 0) -> None:
         self.url = url
         self.priority = priority
-
-    async def getIPAsync(self, context: IPRetrieverContext) -> IPResultObject:
-        session = context.session
-        timeout = aiohttp.ClientTimeout(context.timeout)
-
-        ip = None
-        try:
-            async with session.get(self.url, timeout=timeout) as response:
-                if response.status == 200:
-                    ip = (await response.text()).strip()
-        except Exception as e:
-            logger.warning(
-                f"Run into error making API call to {self.url} due to error: {e}"
-            )
-        return IPResultObject(IPObject(ip), priority=self.priority, retriever=self)
+        super().__init__(url, lambda text: text, priority)
