4b7fa1fcf9624b7e74ec44ea15d27dca3ac6bdd3
catwang01 <edwardlericwzx@gmail.com>
2024-12-16 20:59:27 +0800

    better help messages

--- python_ifconfig_me/cli.py
+++ python_ifconfig_me/cli.py
@@ -52,18 +52,18 @@ def getArgs(raw_args) -> Optional[CommandLineArgs]:
         default=logging.ERROR,
         help="Logging level, can be either a string or positive integer. The string or integer has the same sematic as in the Python's standard logging library. Valid string: [DEBUG, INFO, WARNING, ERROR, CRITICAL]",
     )
-    parser.add_argument("--show-statistics", action="store_true", default=False)
+    parser.add_argument("--show-statistics", action="store_true", default=False, help="Show details about voting")
     parser.add_argument(
         "--ipv6",
         action="store_true",
         default=False,
-        help="Return IPv6 address only. By default, either IPv4 or IPv6 will be returned.",
+        help="Return IPv6 address only. By default, no preference between IPv4 and IPv6.",
     )
     parser.add_argument(
         "--ipv4",
         action="store_true",
         default=False,
-        help="Return IPv4 address only. By default, either IPv4 or IPv6 will be returned.",
+        help="Return IPv4 address only. By default, no preference between IPv4 and IPv6.",
     )
     parser.add_argument(
         "--prefer-ipv6",
@@ -71,7 +71,7 @@ def getArgs(raw_args) -> Optional[CommandLineArgs]:
         default=False,
         help="Prefer IPv6 over IPv4. By default, prefer IPv4 over IPv6, which means choose IPv4 when the IPv4 and IPv6 have the same frequency. Note that, the preference only matters when a IPv4 and IPv6 have the same frequency. Use this flag to override the default behavior.",
     )
-    parser.add_argument("--timeout", type=int, default=5, help="Timeout for API call.")
+    parser.add_argument("--timeout", type=int, default=5, help="Timeout for underlying API calls.")
     args = parser.parse_args(raw_args, namespace=CommandLineArgs())
     if args.ipv4 and args.ipv6:
         print("--ipv4 and --ipv6 can't be used together")
